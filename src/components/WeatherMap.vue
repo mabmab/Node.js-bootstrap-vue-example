00345ee0a97db0c57f9dd591cfc9f0151f405cd9
mabmab <mabmabgg@gmail.com>
2020-07-12 03:48:33 +0900

    wait spinner表示を追加

--- src/components/WeatherMap.vue
+++ src/components/WeatherMap.vue
@@ -1,18 +1,20 @@
 <!-- テンプレート -->
 <template>
   <div class="weather-map">
-    <b-card
-      :img-src="getImgUrl(icon)"
-      v-bind:img-alt="icon"
-      img-top
-      v-bind:title="city"
-      class="mb-2"
-    >
-      <p>Weather: {{weather}}</p>
-      <p>Temperature: {{temperature}}</p>
-      <p>Humidity: {{humidity}}</p>
-      <p>Pressure: {{pressure}}</p>
-    </b-card>
+    <b-overlay :show="is_busy" rounded="sm">
+      <b-card
+        :img-src="getImgUrl(icon)"
+        v-bind:img-alt="icon"
+        img-top
+        v-bind:title="city"
+        class="mb-2"
+      >
+        <p>Weather: {{weather}}</p>
+        <p>Temperature: {{temperature}}</p>
+        <p>Humidity: {{humidity}}</p>
+        <p>Pressure: {{pressure}}</p>
+      </b-card>
+    </b-overlay>>
   </div>
 </template>
 
@@ -22,11 +24,14 @@ import axios from 'axios';
 var icon_sunny = 'f_f_traffic_7_s512_f_traffic_7_0bg';
 var icon_clouds = 'f_f_traffic_4_s512_f_traffic_4_0bg';
 var icon_rain = 'f_f_traffic_5_s512_f_traffic_5_0bg';
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec)); // sleep(msec)
 
 export default {
   name: 'WeatherMap',
   data() {
     return {
+        // busyフラグ
+        is_busy: false,
         // Jsonのデータを格納
         weather: '???',
         temperature : '???',
@@ -40,33 +45,36 @@ export default {
   },
   methods: {
     async get_weather_map() {
-        const abszero = -273.15; // 絶対零度
-        var URL = 'https://api.openweathermap.org/data/2.5/weather';
-        const params = {
-            q: `${this.city},jp`,
-            APPID: process.env.VUE_APP_OPENWEATHERMAP_APIKEY
-        }
-        await axios.get(URL, { params })
-        // thenで成功した場合の処理をかける
-        .then(response => {
-            console.log(`status: ${response.status}`);
-            console.log(`location: ${response.data.name}`);
-            console.log(`date: ${new Date(response.data.dt*1000).toLocaleDateString('ja-jp')}`);
-            console.log(`time: ${new Date(response.data.dt*1000).toLocaleTimeString('ja-jp')}`);
-            console.log('-----');
-            console.log('body:', response.data);
-            this.weather = response.data.weather[0].main;
-            if (this.weather === 'Rain') this.icon = icon_rain;
-            else if (this.weather === 'Clouds') this.icon = icon_clouds;
-            else this.icon = icon_sunny;
-            this.temperature = `${(response.data.main.temp + abszero).toFixed(2)}(C)`;
-            this.humidity = `${(response.data.main.humidity)}(%)`;
-            this.pressure = `${(response.data.main.pressure)}(hp)`;
-        }
-        // catchでエラー時の挙動を定義する
-        ).catch(err => {
-            console.log('err:', err);
-        });
+      const abszero = -273.15; // 絶対零度
+      var URL = 'https://api.openweathermap.org/data/2.5/weather';
+      const params = {
+        q: `${this.city},jp`,
+        APPID: process.env.VUE_APP_OPENWEATHERMAP_APIKEY
+      }
+      this.is_busy = true;
+      await sleep(Math.floor(Math.random() * 4000) + 1000); // 1～5秒の待ち
+      await axios.get(URL, { params })
+      // thenで成功した場合の処理をかける
+      .then(response => {
+        console.log(`status: ${response.status}`);
+        console.log(`location: ${response.data.name}`);
+        console.log(`date: ${new Date(response.data.dt*1000).toLocaleDateString('ja-jp')}`);
+        console.log(`time: ${new Date(response.data.dt*1000).toLocaleTimeString('ja-jp')}`);
+        console.log('-----');
+        console.log('body:', response.data);
+        this.weather = response.data.weather[0].main;
+        if (this.weather === 'Rain') this.icon = icon_rain;
+        else if (this.weather === 'Clouds') this.icon = icon_clouds;
+        else this.icon = icon_sunny;
+        this.temperature = `${(response.data.main.temp + abszero).toFixed(2)}(C)`;
+        this.humidity = `${(response.data.main.humidity)}(%)`;
+        this.pressure = `${(response.data.main.pressure)}(hp)`;
+        this.is_busy = false;
+      }
+      // catchでエラー時の挙動を定義する
+      ).catch(err => {
+        console.log('err:', err);
+      });
     },
     getImgUrl(name) {
       var images = require.context('../assets/', false, /f_f_traffic.+\.png$/);
